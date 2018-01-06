2e6cf1be4492346088ad9b0773c0af2b44af4040
thenups <nn.mathur@gmail.com>
2018-01-06 08:26:50 -0800

    added relevant variables to query

--- censusdata.py
+++ censusdata.py
@@ -9,7 +9,7 @@ for x in range(2011,2017):
 
 apiKey = 'a9bba28cbc522f8f9d8ae3b88ef030fba6034516'
 baseURL = 'https://api.census.gov/data/2016/acs/acs1/'
-getArgs = 'B00001_001E'
+getArgs = 'B25121_001E,C15002_001E'
 forArgs = 'county'
 URLArgs = '?get={}&for={}:*&key={}'.format(getArgs,forArgs,apiKey)
 
@@ -17,3 +17,7 @@ URLArgs = '?get={}&for={}:*&key={}'.format(getArgs,forArgs,apiKey)
 queryURL = baseURL + URLArgs
 
 print(queryURL)
+
+#HOUSEHOLD INCOME IN THE PAST 12 MONTHS (IN 2016 INFLATION-ADJUSTED DOLLARS) BY VALUE
+
+# SEX BY EDUCATIONAL ATTAINMENT FOR THE POPULATION 25 YEARS AND OVER
